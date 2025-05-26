efc1ae9fb7e21423bd0ed145c615762b9295cbe4
oniyudi <onimaru.yudi@gmail.com>
2025-05-26 16:46:21 -0300

    corpo da tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-slate-500 flex flex-col items-center h-screen w-screen p-1 gap-5">
+      <div className="font-bold text-3xl">
+        VOTAÇÃO BINÁRIA
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="border-2 rounded-2xl h-1/2 w-1/2 bg-slate-400 p-2 flex flex-col items-center text-lg gap-10">
+        <div className="flex flex-col w-full gap-1">
+          <span>Informe o tema a ser votado:</span>
+          <input type="text" name="descricao" className="bg-zinc-300 rounded"/>
+        </div>
+        <div className="flex justify-between w-5/6">
+            <div className="border-2 w-1/3 rounded-md flex flex-col items-center p-2 gap-2">
+              opção 1
+              <input type="text" name="op-1" className="w-full bg-zinc-300 rounded"/>
+            </div>
+            <div className="border-2 w-1/3 rounded-md flex flex-col items-center p-2 gap-2">
+              opção 2
+              <input type="text" name="op-2" className="w-full bg-zinc-300 rounded"/>
+            </div>
+        </div>
+        <button type="submit" className="border-1 rounded-md p-2 cursor-pointer bg-slate-600 text-zinc-300">Começar votação</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/pages/Principal.tsx
@@ -0,0 +1,9 @@
+function Principal() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Principal
